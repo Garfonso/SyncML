0d5a023c65e2e5aa474ee3befad21261f5d1599f
Garfonso <achim@tratschtante.de>
2012-05-21 20:26:17 +0200

    repaired build script.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 del info.mobo.syncml_0.1.*_all.ipk
 call palm-package package application srv account
-call palm-install info.mobo.syncml_*_all.ipk
+call palm-install info.mobo.syncml_0.1.*_all.ipk
 REM call palm-launch -p "{mojoConfig: {debuggingEnabled:true}}" info.mobo.syncml
