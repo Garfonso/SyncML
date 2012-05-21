82d08ac8d443374f1f22bcdc7313d09b8ca6a3e3
Garfonso <achim@tratschtante.de>
2012-05-21 21:49:51 +0200

    Change was necessary to allow modifyAccount calls in storeAccounts to return. We don't really use the webOS stored credentials anyway... no idea how to read them.

--- srv/assistants/onCredentialsChangedAssistant.js
+++ srv/assistants/onCredentialsChangedAssistant.js
@@ -5,14 +5,14 @@ onCredentialsChangedAssistant.prototype.run = function (outerFuture) {
   log("Params: " + JSON.stringify(this.controller.args)); //may contain password.
   log("Future: " + JSON.stringify(outerFuture.result));
   
-  if (locked === true) {
-    log("Locked... already running?");
-    previousOperationFuture.then(this, function (f) {
-      log("PreviousOperation finished " + JSON.stringify(f.result) + " , starting onCredentialsChangedAssistant");
-      this.run(outerFuture);
-    });
-    return;
-  }
+//  if (locked === true) {
+//    log("Locked... already running?");
+//    previousOperationFuture.then(this, function (f) {
+//      log("PreviousOperation finished " + JSON.stringify(f.result) + " , starting onCredentialsChangedAssistant");
+//      this.run(outerFuture);
+//    });
+//    return;
+//  }
   
   //this assistant is not helpfull.. it only get's the accountId, but no new credentials??? Why??? :(
   finishAssistant(outerFuture, { returnValue: false, success: false });
