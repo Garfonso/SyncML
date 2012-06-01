8c982871ab13c48697779099e4a8f5563772b4e6
Garfonso <Garfonso@mobo.info>
2012-06-01 10:42:19 +0200

    added build script for linux systems

---
+++ build.sh
@@ -0,0 +1,5 @@
+rm info.mobo.syncml_0.1.*_all.ipk
+palm-package package application srv account
+palm-install info.mobo.syncml_0.1.*_all.ipk
+#palm-launch -p "{mojoConfig: {debuggingEnabled:true}}" info.mobo.syncml
+
