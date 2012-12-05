591a4cf45d2e01da184769517eeae09359266e0c
Garfonso <achim@tratschtante.de>
2012-12-05 23:21:32 +0100

    fixed a hang on account creation

--- srv/lib/eventCallbacks.js
+++ srv/lib/eventCallbacks.js
@@ -183,6 +183,7 @@ var eventCallbacks = (function () {
             logError_lib(e);
           }
         }
+        delete input.result; //try to free some memory.
       }
       if (updates === 0) {//had no iCal conversions.
         input.callback2({add: add, del: del, replace: update, success: input.success}); //still can't distinguish between new and changed items.
@@ -268,17 +269,16 @@ var eventCallbacks = (function () {
 		},
 
 		checkCalendar: function (account) {
-		  var query, calendar, obs, resfuture = new Future({returnValue: false});
+		  var query, calendar, obs, resfuture = new Future();
 			query = { from: "info.mobo.syncml.calendar:1", where: [ { prop: "accountId", op: "=", val: account.accountId } ] };
 
 			//log("Check calendar.");
 			if (!account && !account.accountId) {
 			  //log("Did not get account! => failure.");
-			  return resfuture;
-			}      
-			if (account.datastores.calendar && account.datastores.calendar.enabled) {
+			  resfuture.result = { returnValue: false };
+			} else if (account.datastores.calendar && account.datastores.calendar.enabled) {
         //log("Have Calendar Id: " + account.datastores.calendar.dbId);
-        resfuture = DB.find(query, false, false).then(
+        DB.find(query, false, false).then(
           function (future) {
             var result = future.result, results, i, calPresent = false;
             if (result.returnValue === true) {
@@ -286,14 +286,14 @@ var eventCallbacks = (function () {
               for (i = 0; i < results.length; i += 1) {
                 if (results[i]._id === account.datastores.calendar.dbId) {
                   log("Found calendar, everything ok.. :)");
-                  future.result = {returnValue: true};
+                  resfuture.result = {returnValue: true};
                   calPresent = true;
                 }
               }
               if (!calPresent && results.length > 0) {
                 account.datastores.calendar.dbId = results[0]._id;
                 calPresent = true;
-                future.result = {returnValue: true};
+                resfuture.result = {returnValue: true};
                 log("Calendar was not associated with our account object...? Repaired that.");
               }
             }
@@ -312,7 +312,7 @@ var eventCallbacks = (function () {
               };
               obs = [calendar];
 
-              resfuture = DB.put(obs).then(
+              DB.put(obs).then(
                 function (f1) {
                   if (f1.result.returnValue === true) {
                     log("Created calendar: " + JSON.stringify(f1.result.results));
@@ -320,30 +320,20 @@ var eventCallbacks = (function () {
                       account.datastores.calendar.dbId = f1.result.results[0].id;
                       SyncMLAccount.setAccount(account);
                       SyncMLAccount.saveConfig();
-                      f1.result = {returnValue: true};
+                      resfuture.result = {returnValue: true};
                     } else {
                       log("Error: Add returned no ID??");
-                      f1.result = {returnValue: false};
+                      resfuture.result = {returnValue: false};
                     }
                   } else {
                     log("Could not add calendar: " + f1.result.errorCode + " = " + f1.result.errorMessage);
-                    f1.result = {returnValue: false};
+                    resfuture.result = {returnValue: false};
                   }
                 }
               );
-              resfuture.onError(function (f) {
-                log("Error in checkCalendar-future: " + f.exeption);
-                logToApp("Could not create Calendar: " + JSON.stringify(f.exeption));
-                resfuture.result = { returnValue: false };
-              });
             }
           }
         );
-        resfuture.onError(function (f) {
-          log("Error in checkCalendar-future: " + f.exeption);
-          logToApp("Could not find Calendar: " + JSON.stringify(f.exeption));
-          resfuture.result = { returnValue: false };
-        });
       } else { //calendar disabled => don't create, all is fine.
         resfuture.result = { returnValue: true };
       }
